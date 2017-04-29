99c763b9cf194af3464d346088557ae92a04ae1b
Erik Johnson <mindovermiles262@gmail.com>
2017-04-29 01:35:04 -0700

    split dropdown button navbar

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$('.navbar-collapse .navbar-hide').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+$('.content').click(function() {
+    $(".navbar-collapse").collapse('hide');
+})
+
+$('.navbar-brand').click(function() {
+    $(".navbar-collapse").collapse('hide');
+})
