d95d073840cb080d4571f4e1cbe58c5362c31016
Andy Duss <mindovermiles262@gmail.com>
2018-03-22 17:38:25 -0700

    update skills section

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,9 @@ function setNav() {
   if($(window).width() > 767) {
     $('.nav-heading').removeAttr('data-toggle').removeAttr('data-target');
     $('.nav-heading').addClass("hidden-xs").removeClass("visible-xs");
-    console.log("Hello wide screen");
   } else {
     $('.nav-heading').attr('data-toggle', 'collapse').attr('data-target', '.navbar-collapse');
     $('.nav-heading').addClass("visible-xs").removeClass("hidden-xs");
-    console.log("Hello Small Screen");
   }
 }
 
