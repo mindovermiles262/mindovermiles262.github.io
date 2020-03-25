cc3a99b53277bbd2ccaae019ed34e6f05357f059
Andy D <mindovermiles262@gmail.com>
2020-03-25 16:55:30 +0000

    ironing out bugs

--- src/help.sh
+++ src/help.sh
@@ -1,24 +1,29 @@
-#!/usr/bin/env bash
-printf "[*] Shell: "
+printf "[+] Shell: "
 echo $0
 
-printf "[*] OS: "
+printf "[+] OS: "
 uname -a 
 
-printf "[*] Ruby Version: "
+printf "[+] Ruby Version: "
 ruby -v
 
-printf "[*] Which Ruby: "
+printf "[+] Which Ruby: "
 which ruby
 
-printf "[*] Rbenv Version: "
+printf "[+] Rbenv Version: "
 rbenv -v
 
-printf "[*] Installed Ruby (rbenv) Versions: \n"
+echo "[+] Installed Ruby (rbenv) Versions:"
 rbenv versions | sed -e $'s/^/\t/'
 
-echo "[*] Bash Profile:"
+echo "[+] Bash Profile:"
 tail -n 10 ~/.bash_profile | sed -e $'s/^/\t/'
 
-echo "[*] BashRC:"
+echo "[+] BashRC:"
 tail -n 10 ~/.bashrc | sed -e $'s/^/\t/'
+
+echo "[+] ZshRC:"
+tail -n 10 ~/.zshrc | sed -e $'s/^/\t/'
+
+echo "[+] Which Rails"
+which rails
